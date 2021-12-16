1860bedc8a94f41420d67f4857163de76ccf744b
MitchellLee-HSW <MitchellLee-HSW@users.noreply.github.com>
2021-12-16 02:15:01 +0000

    deploy: b1fc8e5b5b1d9b529729a887a46abe2c03e2a862

---
+++ _next/static/9OQIaKf0THUz1Jb5knZiv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/620008f10752807c41e9.css","static/chunks/pages/index-10012b8c74cd5b2e6139.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
